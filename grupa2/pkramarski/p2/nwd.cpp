cc5eb3824ec52856fc16910812a7aef03ffaaf88
pkramarski <pawel.kramarski@student.uw.edu.pl>
2016-04-03 15:25:57 +0200

    Create nwd.cpp

---
+++ grupa2/pkramarski/p2/nwd.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int NWD(int a, int b) {
+    int r = a % b;
+    while (r != 0) {
+        a = b;
+        b = r;
+        r = a % b;
+    }
+    return b;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int t[n];
+
+    for (int i = 0; i < n; i++)
+    cin >> t[i];
+
+    int nwd = NWD(t[0], t[1]);
+    for(int i = 2; i < n; i++)
+    nwd = NWD(nwd, t[i]);
+
+    cout << nwd;
+}
