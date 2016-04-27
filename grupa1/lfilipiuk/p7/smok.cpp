38af5d2eb8903e9c5c21542dc27af7ea112291cf
lfilipiuk <lukasz.filipiuk@student.uw.edu.pl>
2016-04-27 21:38:27 +0200

    Create smok.cpp

---
+++ grupa1/lfilipiuk/p7/smok.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <math.h>
+#include <cstdlib>
+
+const int w = 800;
+const int h = 600;
+
+int main(int argc, char** argv)
+{
+    ALLEGRO_DISPLAY       *display = NULL;
+
+    al_init();
+    al_init_primitives_addon();
+
+    al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
+    al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
+
+    display = al_create_display(w, h);
+
+    al_clear_to_color(al_map_rgb(0,0,0));
+
+    float x=0;
+    float y=0;
+    for(int i=0;i<10000;i++){
+        if(rand()%2){
+            x=-0.4*x-1;
+            y=-0.4*y+0.1;
+        }
+        else{
+            x=0.76*x-0.4*y;
+            y=0.4*x+0.76*y;
+        }
+        //cout<<"x: "<<x<<"; y: "<<y<<endl;
+        al_draw_pixel((x+1.5)*400 , (y+0.6)*600, al_map_rgb(255,255,255));
+    }
+    al_flip_display();
+
+    al_rest(3);
+    al_destroy_display(display);
+
+    return 0;
+}
