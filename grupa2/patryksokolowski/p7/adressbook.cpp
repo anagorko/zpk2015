458bc7185937f8c5d2e9c6081cff13d797d45d2e
patryksokolowski <patryk.sokolowski@student.uw.edu.pl>
2016-06-07 21:52:26 +0200

    Update adressbook.cpp

--- grupa2/patryksokolowski/p7/adressbook.cpp
+++ grupa2/patryksokolowski/p7/adressbook.cpp
@@ -15,6 +15,7 @@ public:
         data.resize(4);
     };
     record(int id_, string imie, string nazwisko, string email, string telefon){
+        data.resize(4);
         id=id_;
         data[0] = imie;
         data[1] = nazwisko;
@@ -37,6 +38,10 @@ public:
         cout << r.getId() << " " << r.getData(0) << " " << r.getData(1) << " " <<  r.getData(2) << " " << r.getData(3) << endl;
         return os;
     }
+    friend ofstream& operator << (ofstream& os, const record& r){
+        os << r.getId() << " " << r.getData(0) << " " << r.getData(1) << " " <<  r.getData(2) << " " << r.getData(3) << endl;
+        return os;
+    }
 };
 
 class book {
@@ -52,6 +57,9 @@ public:
         }
         record getRecord(int x){
             return r[x];
+        }
+        int size(){
+            return r.size();
         }
         void list_(){
             if (r.size() == 0) cout << "Ksiazka jest pusta" << endl;
@@ -85,20 +93,20 @@ public:
         book sort_(int x){
             vector<record> v = getRecords();
             if (x==-1) {
-                sort(begin(r), end(r), [] (const record& lhs, const record& rhs) { return lhs.getId() < rhs.getId(); });
+                sort(begin(v), end(v), [] (const record& lhs, const record& rhs) { return lhs.getId() < rhs.getId(); });
             }
             switch (x){
                 case 0:
-                    sort(begin(r), end(r), [] (const record& lhs, const record& rhs) { return lhs.getData(0) < rhs.getData(0); });
+                    sort(begin(v), end(v), [] (const record& lhs, const record& rhs) { return lhs.getData(0) < rhs.getData(0); });
                     break;
                 case 1:
-                    sort(begin(r), end(r), [] (const record& lhs, const record& rhs) { return lhs.getData(1) < rhs.getData(1); });
+                    sort(begin(v), end(v), [] (const record& lhs, const record& rhs) { return lhs.getData(1) < rhs.getData(1); });
                     break;
                 case 2:
-                    sort(begin(r), end(r), [] (const record& lhs, const record& rhs) { return lhs.getData(2) < rhs.getData(2); });
+                    sort(begin(v), end(v), [] (const record& lhs, const record& rhs) { return lhs.getData(2) < rhs.getData(2); });
                     break;
                 case 3:
-                    sort(begin(r), end(r), [] (const record& lhs, const record& rhs) { return lhs.getData(3) < rhs.getData(3); });
+                    sort(begin(v), end(v), [] (const record& lhs, const record& rhs) { return lhs.getData(3) < rhs.getData(3); });
                     break;
             }
             return book(v);
@@ -116,6 +124,10 @@ public:
             r[i].setData(3);
         }
         void delete_(int i){
+            if (i>=size()) {
+                    cout << "Nie ma takiego rekordu";
+                    return;
+            }
             for (int j=i;j<r.size()-1;j++){
                 r[j] = r[j+1];
             }
@@ -127,10 +139,10 @@ class UI {
     string filename;
     book file;
 public:
-    UI(){};
     void loadfile(string name){
-        ifstream myfile(filename+".txt");
+        ifstream myfile(name+".txt");
         string line;
+        filename = name;
         if (myfile.is_open())
         {
             while ( getline(myfile,line))
@@ -148,12 +160,12 @@ public:
             file.insertRecord(r);
             }
             myfile.close();
-  }
+        }
     }
     void savefile(string name){
         ofstream bookfile;
         vector<record> bk = file.getRecords();
-        bookfile.open(filename, ios::trunc);
+        bookfile.open(name+".txt", ios::trunc);
          for (record& p: bk) {
             bookfile << p;
         }
@@ -163,6 +175,10 @@ public:
         file.add();
     }
     void deleteRecord(int i){
+        if (i>=file.size()){
+            cout << "Nie ma takiego rekordu!";
+            return;
+        }
         cout << "Removing record: " << file.getRecord(i);
         file.delete_(i);
     }
@@ -171,25 +187,76 @@ public:
         cout << "Record succesfully modified!" << endl;
     }
     void sort_book(int i){
-        book m=file;
-        m.sort_(i);
+        book m;
+        m = file.sort_(i);
         m.list_();
     }
     void help() {
-        cout << "help" << endl;
+        cout << "POMOC: 1. Pierwszy argument to nazwa ksiazki. Na przyklad ZAJECIA book - otwiera ksiazke o nazwie book" << endl;
+        cout << "2. Drugi argument pozwala na wylistowanie (list), dodanie(add), usuniecie(delete), sortowanie(sort), modyfikacja(modyfy)" << endl;
+        cout << "3. Trzeci argument to argument pomocniczy na przyklad do delete (delete 1 - usuwa pierwszy rekord); sort 1 - sortuje po pierwszej kolumnie (id); modify 0 - modyfikuje rekord 0 (pierwszy  w ksiazce)" << endl;
+    }
+    void list_(){
+        file.list_();
     }
 };
 
 int main(int argc, char* argv[]){
-    cout << "jest " << argc << " argumentow" << endl;
+
+   cout << "jest " << argc << " argumentow" << endl;
     for (int i = 0; i < argc; i++) {
         cout << "argument nr " << i << ": " << argv[i] << endl;
     }
-
     UI iface;
-
-    if (argc == 1 || (argc > 1 && string(argv[1]) == "help")) {
+    if (argc == 1 || argc == 2 || (argc > 1 && string(argv[1]) == "help")) {
         iface.help();
+        return 0;
+    }
+    iface.loadfile(string(argv[1]));
+    string drugi = string(argv[2]);
+    if (drugi == "list"){
+        iface.list_();
+        iface.savefile(string(argv[1]));
+        return 0;
+    }
+    if (drugi == "add"){
+        iface.addRecord();
+        iface.savefile(string(argv[1]));
+        return 0;
+    }
+    if (drugi == "delete"){
+        int n = atoi(argv[3]);
+        iface.deleteRecord(n);
+        iface.savefile(string(argv[1]));
+        return 0;
+    }
+    if (drugi == "modify"){
+        int n = atoi(argv[3]);
+        iface.modifyRecord(n);
+        iface.savefile(string(argv[1]));
+        return 0;
+    }
+    if (drugi == "sort"){
+        if (string(argv[3])=="id") {
+            iface.sort_book(-1);
+            return 0;
+        }
+        if (string(argv[3])=="imie") {
+            iface.sort_book(0);
+            return 0;
+        }
+        if (string(argv[3])=="nazwisko") {
+            iface.sort_book(1);
+            return 0;
+        }
+        if (string(argv[3])=="email") {
+            iface.sort_book(2);
+            return 0;
+        }
+        if (string(argv[3])=="telefon") {
+            iface.sort_book(3);
+            return 0;
+        }
         return 0;
     }
 }
