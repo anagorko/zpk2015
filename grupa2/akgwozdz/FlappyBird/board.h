8190f9c08f913e77f80851d65248e9523bcebbc0
akgwozdz <agnieszkaxgwozdz@gmail.com>
2016-09-09 14:19:09 +0200

    Update board.h

--- grupa2/akgwozdz/FlappyBird/board.h
+++ grupa2/akgwozdz/FlappyBird/board.h
@@ -1,6 +1,25 @@
+#include<iostream>
 
-class Bird {
+#include "bird.h"
+#include "obstacle.h"
+
+using namespace std;
+
+const double BIRD_INCREASE_V = 0.8;
+const double GRAVITY = 0.2;
+const double OBSTACLE_SPEED = 2;
+
+class Board {
+
 public:
-    int y;
-    double velocity;
+    int width;
+    int height;
+    Obstacle* obstacles[4];
+    Bird* bird;
+
+    Board(int w, int h);
+
+    void increase_bird_v();
+    int move_objects();
+    bool check_collision();
 };
